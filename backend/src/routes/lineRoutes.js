29408f34b1e6bd3a4d51228c0b4e6b75e2843fb0
KTAP8 <touch.apinankul@gmail.com>
2026-03-26 15:32:26 +0700

    feat: remove Line API integration and associated files.

--- backend/src/routes/lineRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { handleWebhook } = require('../controllers/lineController');
-
-// req.rawBody is set by the express.json verify callback in index.js
-router.post('/webhook', handleWebhook);
-
-module.exports = router;
